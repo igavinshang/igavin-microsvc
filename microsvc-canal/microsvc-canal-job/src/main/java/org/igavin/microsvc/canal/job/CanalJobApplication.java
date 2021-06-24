79a63e9bc2215754ae54059645e5448ddd8a6906
shangguoqiang@xdf.cn <shangguoqiang@xdf.cn>
2021-06-24 17:05:40 +0800

    init canal-job

---
+++ microsvc-canal/microsvc-canal-job/src/main/java/org/igavin/microsvc/canal/job/CanalJobApplication.java
@@ -0,0 +1,13 @@
+package org.igavin.microsvc.canal.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalJobApplication.class, args);
+    }
+
+}
